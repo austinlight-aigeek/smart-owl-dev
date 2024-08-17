ba7dd39a884ac11d2741eea09e47a74f32f25e89
Austin Light <austinlight.fl@gmail.com>
2024-08-16 20:24:34 -0400

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Create the Virtual Environment
+`python3 -m venv venv`
+
+Activating the Virtual Environment
+`source venv/bin/activate`
---
+++ app/main.py
@@ -0,0 +1,7 @@
+import socket
+from os import getenv
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+
